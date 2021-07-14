f112ec6df23dc53b3a8374dfb498e85fb4f0b327
Victor Lupascu <sinajeen@gmail.com>
2021-07-14 12:56:59 -0500

    add functions to create the html and css files

---
+++ utils/generate-site.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+const writeHtmlFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/index.html', fileContent, err => {
+            // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+            if (err) {
+                reject(err);
+                // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+                return;
+            }
+
+            // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+            resolve({
+                ok: true,
+                message: 'HTML File created!'
+            });
+        });
+    });
+};
+
+const writeCssFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/style.css', fileContent, err => {
+            // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+            if (err) {
+                reject(err);
+                // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+                return;
+            }
+
+            // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+            resolve({
+                ok: true,
+                message: 'CSS File created!'
+            });
+        });
+    });
+};
+
+module.exports = { writeHtmlFile, writeCssFile};
