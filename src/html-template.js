6cd69f23b0828e0f8eb5a2ed7d7790484ed5754d
Victor Lupascu <sinajeen@gmail.com>
2021-07-16 14:31:36 -0500

    update the html template

--- src/html-template.js
+++ src/html-template.js
@@ -1,110 +1,65 @@
-// function to iterate thru managers array
-const generateManagers = managersArr => {
-    return `
-        <!-- Managers card -->
-        <div class="row justify-content-around" id="managers">
-        ${managersArr
-            .map(({ name, id, email, officeNumber }) => {
-                return `        
+const writeFile = require('../utils/generate-site');
+
+const generateEmployee = templateArray => {
+    let textString = '';
+    console.log(templateArray);
+    for (let i = 0; i < templateArray.length; i++) {
+
+        if (templateArray[i].role === "Intern") {
+            var icon = '<i class="fas fa-user-graduate"></i>';
+            var htmlString = `
+            <li class="list-group-item">
+                <i class="fas fa-school"></i>
+                <span class="school">:${school}</span>
+            </li>
+            `;
+        } else if (templateArray[i].role === "Manager") {
+            var icon = '<i class="fas fa-mug-hot"></i>';
+            var htmlString = `
+            <li class="list-group-item">
+                <i class="fas fa-phone"></i>
+                <span class="office-number">:${officeNumber}</span>
+            </li>`;
+        } else {
+            var icon = '<i class="fas fa-glasses"></i>';
+            var htmlString = `
+            <li class="list-group-item email-link">
+                <a href="${github}" target="_blank">
+                <i class="fab fa-github"></i> Github</a>
+            </li>
+            `;
+        }
+
+
+        textString += `
+        <div class="row justify-content-around">         
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">${name}</h5>
+                    <h5 class="card-title">${templateArray[i].name}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">
-                        <i class="fas fa-mug-hot"></i> Manager
+                       ${icon} ${templateArray[i].role}
                     </h6>
                     <ul class="list-group">
                         <li class="list-group-item">
                             <i class="fas fa-id-card"></i>
-                            <span class="id-number">:${id}</span>
+                            <span class="id-number">: ${templateArray[i].id}</span>
                         </li>
-                        <li class="list-group-item">
-                            <i class="fas fa-phone"></i>
-                            <span class="office-number">:${officeNumber}</span>
-                        </li>
+                        ${htmlString}
                         <li class="list-group-item email-link">
-                            <a href="mailto:${email}">
+                            <a href="mailto:${templateArray[i].email}">
                                 <i class="fas fa-envelope-open-text"></i> Send Email</a>
                         </li>
                     </ul>
                 </div>
-            </div>`
-            }).join('')}
-        </div><br>
-        `
-};
-
-// function to iterate thru engineers array
-const generateEngineers = engineersArr => {
-    return `
-        <!-- Engineers cards -->
-        <div class="row justify-content-around" id="engineers">
-        ${engineersArr
-            .map(({ name, id, email, github }) => {
-                return `
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">${name}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">
-                        <i class="fas fa-glasses"></i> Engineer
-                    </h6>
-                    <ul class="list-group">
-                        <li class="list-group-item">
-                            <i class="fas fa-id-card"></i>
-                            <span class="id-number">:${id}</span>
-                        </li>
-                        <li class="list-group-item email-link">
-                            <a href="${github}" target="_blank">
-                                <i class="fab fa-github"></i> Github</a>
-                        </li>
-                        <li class="list-group-item email-link">
-                            <a href="mailto:${email}">
-                                <i class="fas fa-envelope-open-text"></i> Send Email</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>`
-            }).join('')}
-        </div><br>
-        `
-};
-
-// function to iterate thru managers array
-const generateInterns = internsArr => {
-    return `
-     <!-- Interns cards -->
-        <div class="row justify-content-around" id="interns">
-        ${internsArr
-        .map(({name,id,email,school}) => {
-            return `
-             <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">${name}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">
-                        <i class="fas fa-user-graduate"></i> Intern
-                    </h6>
-                    <ul class="list-group">
-                        <li class="list-group-item">
-                            <i class="fas fa-id-card"></i>
-                            <span class="id-number">:${id}</span>
-                        </li>
-                        <li class="list-group-item">
-                            <i class="fas fa-school"></i>
-                            <span class="school">:${school}</span>
-                        </li>
-                        <li class="list-group-item email-link">
-                            <a href="mailto: ${email}">
-                                <i class="fas fa-envelope-open-text"></i> Send Email</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>`
-        }).join('')}           
+            </div>
         </div>
-    `
-}
+        `;
+    }
+    return textString;
+};
 
-module.exports = htmltemplate => {
-    const { project, managers, engineers, interns } = htmltemplate;
+const generateHtml = htmlTemp => {
+    console.log(htmlTemp);
     return `<!DOCTYPE html>
 <html lang="en">
 
@@ -116,22 +71,30 @@ module.exports = htmltemplate => {
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
-    <title>${project}</title>
+    <title>Team Profile</title>
 </head>
 
 <body>
     <!-- Header -->
     <nav class="navbar">
         <div class="container-fluid justify-content-around">
-            <a class="navbar-brand" href="#">${project} Team</a>
+            <a class="navbar-brand" href="#">My Team</a>
         </div>
     </nav><br>
     <div class="container-fluid overflow-hidden">
-        ${generateManagers(managers)}
-        ${generateEngineers(engineers)}     
-        ${generateInterns(interns)}
+         ${generateEmployee(templateArray)}
     </div>
 </body>
 
 </html>`
+};
+
+const handler = (templateArray) => {
+
+    const file = generateIndex(templateArray)
+    writeFile(file);
+    console.log('success!')
+
 }
+
+module.exports = handler;
